a9ab8b4e57a10d999ca006e6cd9adf24ae60a89f
Tim Austin <tim@neenjaw.com>
2019-10-12 10:56:04 -0600

    bash script used to run tests

---
+++ bin/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+extended_data="./extended_test_data"
+
+for test_dir in "${extended_data}"/*
+do
+  number=$(basename "$test_dir")
+  # touch "./mock_data/lib/${number}"
+  echo "$test_dir"
+  cp "${test_dir}/two_fer.ex" ./test_data/mock_data/lib/
+
+  ./bin/elixir_analyzer two-fer ./test_data/mock_data/
+
+  mv ./test_data/mock_data/analyze.json "$test_dir"
+  rm ./test_data/mock_data/lib/two_fer.ex
+done
